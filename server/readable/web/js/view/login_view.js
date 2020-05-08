bf06cb3f5a364dc2dab13ea5ecb84a643793150b
MISAKA Takashi <misaka@nec.com>
2020-05-08 18:48:32 +0900

    実行されない文を削除

--- server/readable/web/js/view/login_view.js
+++ server/readable/web/js/view/login_view.js
@@ -150,7 +150,8 @@ function LoginView() {
             $('#loginErrMsg').text(Resource.getMessage('skipLoginErrMsg'));
             return false;
         }
-        return true;
+        // This statement is unreachable.
+        // return true;
     };
     _proto._checkTenantname = function() {
         var _objLoginTenant = $('#txtLogintenant');
