94dd2efb1655799e6190a627c46b50014cf068e1
MISAKA Takashi <misaka@nec.com>
2020-05-12 17:03:20 +0900

    使用していないコンポーネントのインポートを削除

--- smart_device/src/Common/Const.js
+++ smart_device/src/Common/Const.js
@@ -14,8 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-
-import React, { Component } from 'react'
+// Unused imports Component, React.
+// import React, { Component } from 'react'
 import {
   Platform,
 } from 'react-native'
